62340e5290384376c1241e6fbe5b090d9decc4d3
hongwei7 <1216869567@qq.com>
2020-12-15 12:23:40 +0800

    update

--- 89.格雷编码.cpp
+++ 89.格雷编码.cpp
@@ -7,23 +7,7 @@
 // @lc code=start
 class Solution {
 public:
-    int getnum(vector<int> now){
-        int result = 0;
-        for(auto i : now){
-            result = 2*result + i;
-        }
-        return result;
-    }
-    void solve(int x, vector<int> now, vector<int>& result){
-        if(x == now.size()){
-            result.push_back(getnum(now));
-            return;
-        }
-        now[x] = 0;
-        solve(x + 1, now, result);
-        now[x] = 1;
-        solve(x + 1, now, result);
-    }
+
     vector<int> grayCode(int n) {
         vector<int> result;
         vector<int> now(n, 0);
